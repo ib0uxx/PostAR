f76bd482af8cfdf7de72a4a8c7abdf304d9271c8
JeanBon-dev <hugues.porchet@orange.fr>
2025-03-25 10:17:27 +0100

    Responsive profilepage

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -7,9 +7,10 @@ import { FaEdit, FaCheckCircle } from 'react-icons/fa';
 const ProfilePage: React.FC = () => {
   return (
     <div className={styles.profileWrapper}>
-      <Navbar />
+      <div className={styles.navbarWrapper}>
+        <Navbar />
+      </div>
       <div className={styles.profileContainer}>
-        {}
         <div className={styles.leftColumn}>
           <h2 className={styles.profileTitle}>My profile</h2>
           <div className={styles.profileCard}>
@@ -23,15 +24,15 @@ const ProfilePage: React.FC = () => {
                 <p className={styles.email}>johndoe@mail.com</p>
               </div>
             </div>
-            <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p className={styles.description}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
           </div>
           <div className={styles.stats}>
             <div className={styles.statBox}><span>6</span> Cards sent</div>
             <div className={styles.statBox}><span>4</span> Cards received</div>
           </div>
         </div>
-        
-        {}
         <div className={styles.rightColumn}>
           <button className={styles.editProfile}><FaEdit /> Edit your profile</button>
           <div className={styles.options}>
